0f75168e1985d04665766605333ff7752fedbec6
hydra13 <rob.sabirov@yandex.ru>
2019-12-21 12:21:25 +0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <NewMessageForm @send="addMessage" />
+    <MessageList :messages="messages" />
+  </div>
+</template>
+
+<script>
+import NewMessageForm from "./components/NewMessageForm";
+import MessageList from "./components/MessageList";
+export default {
+  name: "App",
+  components: {
+    NewMessageForm,
+    MessageList
+  },
+  data() {
+    return {
+      messages: []
+    };
+  },
+  methods: {
+    addMessage(text) {
+      this.messages.unshift(text);
+    }
+  }
+};
+</script>
---
+++ src/components/NewMessageForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input v-model="inputText" type="text" data-test="messageText" />
+    <button @click="send" data-test="sendButton">Add message</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NewMessageForm",
+  data() {
+    return {
+      inputText: ""
+    };
+  },
+  methods: {
+    send() {
+      this.$emit("send", this.inputText);
+      this.inputText = "";
+    }
+  }
+};
+</script>
